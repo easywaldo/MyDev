6f27104cc3303fe4acef8901017790f8733906b3
jinam.lee <jinam.lee@ohmylab.com>
2018-05-02 12:53:20 +0900

    commit

---
+++ aspnetcore_test/aspnetcore_test/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using System;
+
+namespace aspnetcore_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebHost webHost = WebHost
+                .CreateDefaultBuilder(args)
+                .Build();
+
+            webHost.Run();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
